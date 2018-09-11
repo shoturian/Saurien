64d61589a1e4e5261f459feb892ab8298eb3e388
Saurien <39499273+Saurien@users.noreply.github.com>
2018-09-11 20:27:22 +0300

    bot game was changed

--- console/console-log.js
+++ console/console-log.js
@@ -13,6 +13,7 @@ var dev = token.dev;
 var predev = token.predev
 
 module.exports = r4dar => {
+  r4dar.user.setGame('GAME HERE')
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Activating The Bot!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Bot Activated!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Bot Devloper ${dev} and ${predev}`);
