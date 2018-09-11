f936638b7b4eb2e09086d14605aa2bb8c94299b4
Saurien <39499273+Saurien@users.noreply.github.com>
2018-09-11 18:12:27 +0300

    Fix

--- Console/MemberUpdate.js
+++
@@ -1,8 +0,0 @@
-//New Bot
-//
-//by Saurien (aka R4dar) and CpryX Team & YalcnK
-
-const ddiff = require('return-deep-diff');
-module.exports = (oMember, nMember) => {
-  console.log(ddiff(oMember, nMember));
-};
