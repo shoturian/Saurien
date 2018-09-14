88b56fc92cbc8f3f4825835bb9280e789e0d3971
Saurien <39499273+Saurien@users.noreply.github.com>
2018-09-15 02:00:36 +0300

    fix the console errors

--- console/kernel.js
+++ console/kernel.js
@@ -4,7 +4,7 @@
 
 const reqEvent = (event) => require(`../console/${event}`);
 module.exports = r4dar => {
-  r4dar.on('ready', () => reqEvent('console-log')(NewID));
+  r4dar.on('ready', () => reqEvent('console-log')(r4dar));
   r4dar.on('message', reqEvent('response'));
   r4dar.on('guildMemberAdd', reqEvent('bot-login-message'));
   r4dar.on('guildMemberRemove', reqEvent('bot-exit-message'));
