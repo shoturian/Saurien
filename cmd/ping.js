f5d3be07d36714c1fc6a13c8b8cb12fed58c1048
Saurien <39499273+Saurien@users.noreply.github.com>
2018-09-18 01:14:14 -0500

    Little change in the +ping command

--- cmd/ping.js
+++ cmd/ping.js
@@ -9,7 +9,7 @@ exports.run = (r4dar, message) => {
  message.channel.send({embed: {
 		  file:"https://78.media.tumblr.com/be43242341a7be9d50bb2ff8965abf61/tumblr_o1ximcnp1I1qf84u9o1_500.gif",
           color: 0xD97634,
-		  description: "**R4dar's current response traffic: **" + r4dar.ping + "ms" + "\n" + "**Your current response traffic: **" + (new Date().getTime() - message.createdTimestamp) + "ms"
+		  description: "**Your ping: **" + (new Date().getTime() - message.createdTimestamp) + "ms"
             }});
 };
 
