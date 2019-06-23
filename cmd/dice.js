19545cf0d8d2a814df0a52d850df6959d9a9a1fd
Radiaction <39499273+Radiaction@users.noreply.github.com>
2019-06-24 00:16:28 +0300

    New dice command

---
+++ cmd/dice.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const fs = require("fs");
+exports.run = (Saurien, message, params) => {
+var Random = [
+':one:',
+':two:',
+':three:',
+':four:',
+':five:',
+':six:',
+];
+var roll = Math.floor(Math.random()*Random.length);
+const rolll = new Discord.RichEmbed()
+.setDescription('Incoming number: ' + `${Random[roll]}`)
+.setColor(0x036992)
+message.channel.send(rolll)
+};
+exports.conf = {
+enabled: true,
+guildOnly: false,
+aliases: [],
+permLevel: 0
+};
+
+exports.help = {
+name: 'dice',
+};
