1603d5b1fdb66ad7c9a93aa8310467f23b99b49e
Radiaction <39499273+Radiaction@users.noreply.github.com>
2019-06-24 00:06:46 +0300

    new heads or tails game

---
+++ cmd/headsorTails.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const chancejs = require('chance');
+const chance = new chancejs();
+
+const module2 = [
+	"**Tails**",
+	"**Heads**"
+];
+
+exports.run = function(Saurien, message) {
+	
+	var module1 = module2[Math.floor(Math.random() * module2.length)];
+	
+	if (module1 === "**Heads**") {
+		
+		 const embed1 = new Discord.RichEmbed()
+		.setColor(0xf4b942)
+        .setDescription("**Heads**")
+		message.channel.send(embed1);
+		
+	} else if (module1 === "**Tails**") {
+		
+		const embed2 = new Discord.RichEmbed()
+		.setColor(0xf4b942)
+		.setDescription("**Tails**")
+		message.channel.send(embed2);
+		
+	}
+		
+
+};  
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'headsorTails',
+};
