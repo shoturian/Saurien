9a1529de70703829221d24edccbd1d9a53b5d536
Radiaction <39499273+Radiaction@users.noreply.github.com>
2019-06-23 22:57:52 +0300

    New avatar command

---
+++ cmd/avatar.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+
+    const embed = new Discord.RichEmbed()
+        .setAuthor(message.author.tag)
+        .setImage(message.author.avatarURL)
+
+    message.channel.send(embed);
+
+};
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: false, 
+  aliases: [ ],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'avatar', 
+};
