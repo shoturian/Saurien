e028130d4f854e4221083c8e83d14f7b081fb3d4
Radiaction <39499273+Radiaction@users.noreply.github.com>
2020-06-05 20:45:32 +0300

    Later shortened to improve
    Reserved

--- cmd/userInformation.js
+++ cmd/userInformation.js
@@ -7,21 +7,15 @@ const id = require('../id.json');
 
 exports.run = (Saurien, message, params) => {
     if (message.channel.type !== "group") {
-        var Status = message.author.presence.status;
-        var stat1 = (Status == "online" ? (0xD97634) : (Status == "offline" ? (0xD97634) : (Status == "idle" ? (0xD97634) : (Status == "dnd" ? (0xD97634) : (0xD97634)))))
-        var stat2 = (Status == "online" ? ("Online") : (Status == "offline" ? ("Offline") : (Status == "idle" ? ("Idle") : (Status == "dnd" ? ("Do Not Bother") : ("Not Found.")))))
       const Info = new Discord.RichEmbed()
       .setAuthor(Saurien.user.username, Saurien.user.avatarURL)
-      .setColor(stat1)
+	  .setColor('#7d5bbe')
       .setTimestamp()
       .addField('Your Name:', message.author.username + '#' + message.author.discriminator)
       .addField('Your ID:', message.author.id)
       .addField('Sign Date:', message.author.createdAt)
-      .addField('Status:', stat1)
-      .addField('The Game You Played:', message.author.presence.game ? message.author.presence.game.name : 'You Are Not Playing Games')
-      .addField('You Are Bot?', message.author.bot ? '\n Yes' : 'No')
       console.log("userInformation Commands Used By" + message.author.username)
-      return message.channel.sendEmbed(Info);
+      return message.channel.send(Info);
   }
 };
 
