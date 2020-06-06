d4dab0e4f4d9b2a89c9d093327cc4e988edf5d9e
Radiaction <39499273+Radiaction@users.noreply.github.com>
2020-06-06 13:01:57 +0300

    Support server command converted to bot invite command

--- cmd/invite.js
+++ cmd/invite.js
@@ -5,7 +5,6 @@
 const Discord = require('discord.js');
 const id = require('../id.json');
 const Saurien = new Discord.Client();
-var invite = id.invite
 
 exports.run = (Saurien, message) => {
   if (message.channel.type !== 'dm') {
@@ -15,13 +14,13 @@ exports.run = (Saurien, message) => {
     .setAuthor(message.author.username, message.author.avatarURL)
     .setDescription('Check Your DM. :postbox:');
     message.channel.send(invite) }
+    console.log("invite Commands Used By" + message.author.username)
 	const invite1 = new Discord.RichEmbed()
     .setColor(0xD97634)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription(`Link: ${invite}`)
+    .setDescription(`Link: https://discordapp.com/oauth2/authorize?client_id=${Saurien.user.id}&scope=bot&permissions=8`)
     return message.author.send(invite1)
-	console.log("supportServer Commands Used By" + message.author.username);
 };
 
 exports.conf = {
@@ -32,5 +31,5 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'supportServer'
+  name: 'invite'
 };
